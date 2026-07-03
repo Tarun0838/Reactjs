ad43ac9a37a7cb75a76f10fa9a832afb6132d6ab
Tarun Yadav <tarunyadav0838@gmail.com>
2026-07-03 22:14:59 +0530

    day1 of React js

---
+++ day1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello Tarun kaise ho</h1>
+      <h2>Tarun : Backend Developer </h2>
+    </div>
+  )
+}
+
+export default App
