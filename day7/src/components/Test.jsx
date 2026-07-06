da8f0bee9504fb326022b5991b9234f62f735215
Tarun Yadav <tarunyadav0838@gmail.com>
2026-07-06 18:02:16 +0530

    function call

---
+++ day7/src/components/Test.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Test = () => {
+    const scrolling = (e)=> {
+        console.log("Scrolling speed ",e.deltaY)
+    }
+    
+  return (
+    <>
+    <div className='h-screen w-full bg-sky-950' >
+            
+    </div>
+    </>
+  )
+}
+
+export default Test
